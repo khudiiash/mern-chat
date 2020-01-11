1e7e14376c12b9819fff7882f67867af238271ff
Dmytro <dmytro.khudiyash@gmail.com>
2020-01-11 15:20:10 +0200

    init

--- src/core/db.ts
+++ src/core/db.ts
@@ -4,5 +4,4 @@ mongoose.connect(String(process.env.MONGO), {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
-  useUnifiedTopology: true
 }).then(()=>console.log('Mongo connected'));
