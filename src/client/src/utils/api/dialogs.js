a0f86cd503033e84d552e715718571c72ffd164c
Dmytro <dmytro.khudiyash@gmail.com>
2020-01-11 17:05:33 +0200

    init

--- src/client/src/utils/api/dialogs.js
+++
@@ -1,8 +0,0 @@
-import { axios } from "core";
-
-export default {
-  getAll: () => axios.get("/dialogs"),
-  getOneById: id => axios.get(`dialogs/${id}`),
-  create: ({ partner, text }) => axios.post("/dialogs", { partner, text }),
-  delete: id => axios.delete(`/dialogs/${id}`),
-};
--- src/core/db.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-mongoose.connect(String(process.env.MONGO), {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-}).then(()=>console.log('Mongo connected'));
