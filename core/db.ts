cb27794b00e514c69bc7af6642bb2044814b7f01
Dmytro <dmytro.khudiyash@gmail.com>
2020-01-11 17:29:06 +0200

    init

--- core/db.ts
+++ core/db.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect(String(process.env.MONGO), {
+mongoose.connect(String(process.env.MONGO || 'mongodb+srv://Dmytro:149600earthsun@cluster0-mwooj.mongodb.net/chat?retryWrites=true&w=majority'), {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
