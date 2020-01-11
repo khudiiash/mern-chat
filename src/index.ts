fca82d21b974ca0bf7d577fa21977b93bbcfd88f
Dmytro <dmytro.khudiyash@gmail.com>
2020-01-11 20:09:09 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,9 @@ createRoutes(app, io);
 
 const PORT = process.env.PORT || 3003;
 if (process.env.NODE_ENV === 'production') {
+  console.log(process.env.MONGODB_URI)
+  console.log(process.env.JWT_SECRET)
+  
   app.use(express.static( 'client/build '))
   app.get('/*', (req,res) => {
     res.sendFile(path.join(__dirname, 'client','build','index.html'));
