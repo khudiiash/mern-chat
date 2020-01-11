7b73941db87573dc94092996b3a14e1f77eb9d8b
Dmytro <dmytro.khudiyash@gmail.com>
2020-01-11 19:52:15 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ createRoutes(app, io);
 const PORT = process.env.PORT || 3003;
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static( 'client/build '))
-  app.get('*', (req,res) => {
+  app.get('/*', (req,res) => {
     res.sendFile(path.join(__dirname, 'client','build','index.html'));
   })
 }
