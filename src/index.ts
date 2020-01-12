e5ca7a7b1265e33f894c50722d7ba7aa4e8ac1ba
Dmytro <dmytro.khudiyash@gmail.com>
2020-01-12 21:07:00 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path'
-import fs from 'fs'
 import { createServer } from 'http';
 const jwt = require('jsonwebtoken');
-const exjwt = require('express-jwt');
+
 
 dotenv.config();
 
@@ -18,19 +17,18 @@ const io = createSocket(http);
 
 createRoutes(app, io);
 
-const jwtMW = exjwt({
-  secret: process.env.JWT_SECRET
-});
 
 
 const PORT = process.env.PORT || 3003;
 if (process.env.NODE_ENV === 'production') {
-  console.log(process.env)
+
   app.use(express.static(path.join(__dirname, "client", "build")))
 
-  app.get('*', jwtMW, (req,res) => {
+  app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname,'client','build','index.html'));
   })
+  app.use(jwt())
+  
 }
 
 http.listen(PORT, function() {
