2021e8505d047ff506bb3567694596248b85f88e
Dmytro <dmytro.khudiyash@gmail.com>
2020-01-12 21:44:32 +0200

    init

--- src/middlewares/checkAuth.ts
+++ src/middlewares/checkAuth.ts
@@ -13,15 +13,16 @@ export default (req: any, res: any, next: any) => {
 
   const token = req.headers.token;
 
+  console.log('headers')
+  console.log(req.headers)
   console.log('token: '+token)
-  
+
     verifyJWTToken(token)
     .then((user: any) => {
       req.user = user.data._doc;
       next();
     })
     .catch(err => {
-      console.log(err)
       res.status(403).json({ message: err });
     });  
 };
