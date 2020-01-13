8d372fd2dd78d36aa82e29f2846c9baf3dc88961
Dmytro <dmytro.khudiyash@gmail.com>
2020-01-13 10:42:31 +0200

    init

--- client/src/utils/helpers/index.js
+++
@@ -1,6 +0,0 @@
-export { default as validateField } from "./validateField";
-export { default as convertCurrentTime } from "./convertCurrentTime";
-export { default as timeFromNow } from "./timeFromNow";
-export { default as generateAvatarFromName } from "./generateAvatarFromName";
-export { default as openNotification } from "./openNotification";
-export { default as isAudio } from "./isAudio";
